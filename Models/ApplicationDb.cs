d4c79983c4ca05b5e083314bc126aedf8b29a0a8
la237280-Belaroussi <la237280@student.helha.be>
2025-11-02 01:04:13 +0100

    "Feature Gestion des livres développée"

---
+++ Models/ApplicationDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Models
+{
+    public class ApplicationDb : DbContext
+    {
+        public ApplicationDb(DbContextOptions<ApplicationDb> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (!property.IsPrimaryKey() && property.ClrType == typeof(string))
+                    {
+                        property.IsNullable = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookify.Models
+{
+    /// <summary>
+    /// Classe Book reéprésentant un livre dans l'application
+    /// </summary>
+    public class Book
+    {
+        // Attributs de la classe Book
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Le titre est obligatoire.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "L'auteur est obligatoire.")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "ISBN obligatoire.")]
+        public string ISBN { get; set; }
+        [Required(ErrorMessage = "Le prix est obligatoire.")]
+        public int price { get; set; }
+        [Required(ErrorMessage = "La description est obligatoire.")]
+        public string Description { get; set; } 
+        [Required(ErrorMessage = "L'éditeur est obligatoire.")]
+        public string Publisher { get; set; }
+
+        // Foreign key for Gender
+        [ForeignKey("Gender")]
+        public int GenderId { get; set; }
+        // Navigation
+        public Gender gender { get; set; }
+    }
+}
